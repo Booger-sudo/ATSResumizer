18dd4ce8455eae824c02d591c5174eae2da8a9f0
japaul <niightmarejp@gmail.com>
2025-02-14 21:14:32 -0600

    initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+import os from dotenv
+import load_dotenv
+
+#Load enviorment variables 
+load_dotenv()
+
+class Config:
+    UPLOAD_FOLDER = 'uploads/'
+    OPENAI_API_KEY =
+os.getenv("OPENAI_API_KEY")
